16480713dc94b63eea9ac4b63dd074bec5690094
Roni Choudhury <roni.choudhury@kitware.com>
2018-12-19 09:42:04 -0500

    Move vuex store setup into separate directory

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
 import Vue from 'vue';
-import Vuex from 'vuex';
 
 import App from './App.vue';
+import { store } from './store';
 
 Vue.config.productionTip = false;
-
-Vue.use(Vuex);
 
 async function json (url) {
   const response = await fetch(url);
@@ -14,64 +12,6 @@ async function json (url) {
 }
 
 async function main () {
-  const store = new Vuex.Store({
-    state: {
-      roiPlot: {
-        width: 0,
-        height: 0
-      },
-      rois: [],
-      dff: [],
-      epochs: [],
-      focus: [],
-      timeIndex: 0,
-      mode: 'selection'
-    },
-    mutations: {
-      setData (state, { which, data }) {
-        state[which] = data;
-      },
-
-      setROISize (state, dim) {
-        state.roiPlot.width = dim.width;
-        state.roiPlot.height = dim.height;
-      },
-
-      focus (state, which) {
-        state.focus = which;
-      },
-
-      toggle (state, which) {
-        const focus = state.focus;
-
-        // Copy the focus elements over if they do not match the target; this
-        // will toggle that element off if it is current in focus.
-        let out = [];
-        focus.forEach(d => {
-          if (d !== which) {
-            out.push(d);
-          }
-        });
-
-        // If the output length is the same as the input length, then the
-        // element wasn't found and we must therefore turn it on.
-        if (out.length === focus.length) {
-          out.push(which);
-        }
-
-        state.focus = out;
-      },
-
-      mode (state, mode) {
-        state.mode = mode;
-      },
-
-      timeIndex (state, val) {
-        state.timeIndex = val;
-      }
-    }
-  });
-
   // Grab the ROIs and Dff data parallel-asynchronously.
   const roisReq = json('data/rois.json');
   const dffReq = json('data/dff.json');
